22a71ee091352419b9c9edd15a414a6619c72902
Siddhant jha <40361603+siddhantJH@users.noreply.github.com>
2024-12-10 09:07:29 +0000

    first hour

---
+++ 01Basics/chai.py
@@ -0,0 +1,10 @@
+# named it as py
+# now here we need to call the method of helo_python file and run it
+
+# like normal english we import the chai function 
+from hello_python import chai
+
+
+# now as soon as i ran it a new folder got create called as a pycache folder ( 2 __ both side )
+# inside that we have a pyc file now the main reason for that is 
+chai("hello called from another file") 
---
+++ 01Basics/hello_python.py
@@ -0,0 +1,24 @@
+#Best way to install python is using python.org 
+#python ko run kahi bhi karae vo sara ka sara kam aik hi commmunity karti hai 
+#behavior of language does not changes, in JS behavior changes depending upon where you are running it node or browser
+#download the setup according to the operting system 
+#Installs straight forwardly
+#In windows you only have python, however in mac you write python3 to run the code.
+print("hello")
+
+# def stands for definition
+def chai(n):  #when we press enter it gets indentated automatically
+    print(n)
+
+chai(10) #can give integer or string any datatype
+# we can execute the chai function using .notation and file name hello.python in python shell 
+
+
+# decalare various ariable 
+chai_one="lemon tea"
+chai_two="ginder tea"
+chai_three="adrak chai"
+# now if we try to use these variable using the .notation it shows no attribute named as chai_one ,because when we imported it then it was not there. 
+#  so in shell we need to close the shell and reimport it , or rather now we need to reload it on the go. 
+# from importlib import reload   #this reloads the changes done ont he file.
+# write reload(hello_chai) now we can use the attribites using the dot notation.
