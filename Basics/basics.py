7ae6abebba8fccd0e7f1d6f7b19ccaae8edc2f3d
Siddhant jha <40361603+siddhantJH@users.noreply.github.com>
2024-12-10 06:58:13 +0000

    first code

---
+++ Basics/basics.py
@@ -0,0 +1,7 @@
+#Best way to install python is using python.org 
+#python ko run kahi bhi karae vo sara ka sara kam aik hi commmunity karti hai 
+#behavior of language does not changes, in JS behavior changes depending upon where you are running it node or browser
+#download the setup according to the operting system 
+#installs straight forwardly
+# in windows you only have python, however in mac you write python3 to run the code 
+print("hello")
